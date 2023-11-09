8aa09f175f56659f8d393b79a02e302746dc773b
Thiago_Cruz <101190863+Thiago163@users.noreply.github.com>
2023-11-09 16:33:14 -0300

    Update questionario.c

--- codigo_questionario/questionario.c
+++ codigo_questionario/questionario.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MAX_CHAR 100
 #define NUM_PERGUNTAS 8
@@ -14,7 +15,7 @@ typedef struct
     char resposta;
 } Pergunta;
 
-void apresentarPergunta(Pergunta pergunta, FILE *arquivo, int numeroQuestao)
+void apresentarPergunta(Pergunta pergunta, FILE *arquivo, FILE *binario, int numeroQuestao)
 {
     char opcao;
     printf("\nQuestão %d:\n%s\n", numeroQuestao, pergunta.pergunta);
@@ -25,83 +26,126 @@ void apresentarPergunta(Pergunta pergunta, FILE *arquivo, int numeroQuestao)
     }
 
     // Obter resposta do usuário
-    printf("Sua resposta (A, B, C ou D): ");
+    printf("Sua resposta (A, B, C ou D, ou # para parar): ");
     scanf(" %c", &opcao);
+
+    // Verificar se foi digitado #
+    if (opcao == '#')
+    {
+        char continuar;
+        printf("Deseja continuar respondendo perguntas? (S/N): ");
+        scanf(" %c", &continuar);
+
+        // Converter para maiúsculas para facilitar a comparação
+        continuar = toupper(continuar);
+
+        // Se não deseja continuar, verificar a senha
+        if (continuar != 'S')
+        {
+            char senha[20];
+
+            printf("Digite a senha para encerrar o programa: ");
+            scanf("%s", senha);
+
+            // Verificar a senha
+            if (strcmp(senha, "banana") == 0)
+            {
+                exit(0); // Encerrar o programa
+            }
+            else
+            {
+                printf("Senha incorreta. Continuando...\n");
+            }
+        }
+    }
 
     // Armazenar no arquivo CSV
     fprintf(arquivo, "Questão %d; Resposta:; %c;\n", numeroQuestao, opcao);
+
+    // Armazenar no arquivo binário
+    fwrite(&numeroQuestao, sizeof(int), 1, binario);
+    fwrite(&opcao, sizeof(char), 1, binario);
+
     printf("\n");
 }
 
 int main()
 {
     setlocale(LC_ALL, "portuguese");
-    FILE *arquivo;
+    FILE *arquivo, *binario;
     Pergunta perguntas[NUM_PERGUNTAS];
 
-    arquivo = fopen("respostas.csv", "a");
-    if (arquivo == NULL)
+    do
     {
-        perror("Erro ao abrir o arquivo de saída");
-        return 1;
-    }
+        arquivo = fopen("respostas.csv", "a");
+        binario = fopen("binario.bin", "ab");
 
-    FILE *perguntasFile = fopen("perguntas.txt", "r");
-    if (perguntasFile == NULL)
-    {
-        perror("Erro ao abrir o arquivo de perguntas");
-        fclose(arquivo);
-        return 1;
-    }
-
-    // Iterar sobre as perguntas
-    for (int i = 0; i < NUM_PERGUNTAS; i++)
-    {
-        // Ler texto da pergunta
-        fscanf(perguntasFile, " %[^\n]%*c", perguntas[i].pergunta);
-
-        // Ler opções de resposta
-        for (int j = 0; j < 4; j++)
+        if (arquivo == NULL || binario == NULL)
         {
-            fscanf(perguntasFile, " %[^\n]%*c", perguntas[i].opcoes[j]);
+            perror("Erro ao abrir o arquivo de saída");
+            return 1;
         }
 
-        // Ler resposta correta
-        fscanf(perguntasFile, " %c%*c", &perguntas[i].resposta);
-    }
+        FILE *perguntasFile = fopen("perguntas.txt", "r");
+        if (perguntasFile == NULL)
+        {
+            perror("Erro ao abrir o arquivo de perguntas");
+            fclose(arquivo);
+            fclose(binario);
+            return 1;
+        }
 
-    for (int i = 0; i < NUM_PERGUNTAS; i++)
-    {
-        printf("Bem-vindo ao Museu Multitemático!!! \n");
-        printf("   ______________________________   \n");
-        printf("  /                             /\\ \n");
-        printf(" /_____________________________/  \\\n");
-        printf("|                              |   |\n");
-        printf("|  [ ]                    [ ]  |   |\n");
-        printf("|              [ ]             |   |\n");
-        printf("|  [ ]                    [ ]  |   |\n");
-        printf("|              [ ]             |   |\n");
-        printf("|  [ ]                    [ ]  |   |\n");
-        printf("|______________________________|   |\n");
-        printf("|          MUSEU               |   |\n");
-        printf("|       MULTITEMATICO          |   |\n");
-        printf("|______________________________|   |\n");
-        printf("|          |    | |            |   |\n");
-        printf("|          |    | |            |   |\n");
-        printf("|__________|____|_|____________|___|\n\n");
-        apresentarPergunta(perguntas[i], arquivo, i + 1);
-        system("pause");
-        system("cls");
-    }
+        // Iterar sobre as perguntas
+        for (int i = 0; i < NUM_PERGUNTAS; i++)
+        {
+            // Ler texto da pergunta
+            fscanf(perguntasFile, " %[^\n]%*c", perguntas[i].pergunta);
 
-    // Adicionar uma quebra de linha no final de cada conjunto de respostas
-    fprintf(arquivo, "\n");
+            // Ler opções de resposta
+            for (int j = 0; j < 4; j++)
+            {
+                fscanf(perguntasFile, " %[^\n]%*c", perguntas[i].opcoes[j]);
+            }
 
-    // Fechar arquivos
-    fclose(arquivo);
-    fclose(perguntasFile);
+            // Ler resposta correta
+            fscanf(perguntasFile, " %c%*c", &perguntas[i].resposta);
+        }
 
-    printf("Respostas salvas com sucesso no arquivo respostas.csv\n");
+        for (int i = 0; i < NUM_PERGUNTAS; i++)
+        {
+            printf("Bem-vindo ao Museu Multitemático!!! \n");
+            printf("   ______________________________   \n");
+            printf("  /                             /\\ \n");
+            printf(" /_____________________________/  \\\n");
+            printf("|                              |   |\n");
+            printf("|  [ ]                    [ ]  |   |\n");
+            printf("|              [ ]             |   |\n");
+            printf("|  [ ]                    [ ]  |   |\n");
+            printf("|              [ ]             |   |\n");
+            printf("|  [ ]                    [ ]  |   |\n");
+            printf("|______________________________|   |\n");
+            printf("|          MUSEU               |   |\n");
+            printf("|       MULTITEMATICO          |   |\n");
+            printf("|______________________________|   |\n");
+            printf("|          |    | |            |   |\n");
+            printf("|          |    | |            |   |\n");
+            printf("|__________|____|_|____________|___|\n\n");
+            apresentarPergunta(perguntas[i], arquivo, binario, i + 1);
+            system("pause");
+            system("cls");
+        }
+
+        // Adicionar uma quebra de linha no final de cada conjunto de respostas
+        fprintf(arquivo, "\n");
+
+        // Fechar arquivos
+        fclose(arquivo);
+        fclose(binario);
+        fclose(perguntasFile);
+
+        printf("Respostas salvas com sucesso no arquivo respostas.csv\n");
+
+    } while (1);
 
     return 0;
 }
